e02f162a146e99c6998dcd55ef8123e7d9b03a81
Vladimir Chebakov <chebakovvl@gmail.com>
2019-04-25 22:01:56 +0300

    sparta

--- telegram-charts/server.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var app = express();
-var indexRoute = require('./index');
-app.use(function (req, res, next) {
-  indexRoute(req, res, next);
-});
-app.listen(9000);
